3118d31f447f7bc6f532d99273ef239e7834f2df
Hugo Lemerle <hlml200294@gmail.com>
2022-03-01 22:01:57 +0100

    Routes created

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+    .connect("mongodb+srv://userTest:Thssld2002@cluster0.bvpii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+});
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ backend/routes/user.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User');
+
+router.post('/signup',(req, res, next) => {
+    const user = new User({
+        ...req.body
+    });
+    user.save()
+        .then(() => res.status(201).json({message: 'Inscription réussie !'}))
+        .catch(error => {
+            res.status(400).json({error});
+            console.log('Error !');
+        });
+});
+
+module.exports = router;
