24b1cdd1711e6d73a3c12e64d77cd274adf841c9
Hugo Lemerle <hlml200294@gmail.com>
2022-03-07 18:26:45 +0100

    Like Dislike gestion

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -58,3 +58,100 @@ exports.deleteSauce = (req, res, next) => {
         })
         .catch(error => res.status(500).json({error}));
 }
+
+exports.likeDislike = (req, res, next) => {
+    
+    let like = req.body.likes;
+    let userId = req.body.userId;
+    let sauceId = req.params.id;
+    console.log(req.body.likes);
+
+    Sauce.findOne({_id: sauceId})
+        .then(sauce => {
+            let hasTheUserLikedThisSauce = sauce.usersLiked.includes(userId);
+            let hasTheUserDislikedThisSauce = sauce.usersDisliked.includes(userId);
+
+            switch (like) {
+                case 1:
+                    if (hasTheUserLikedThisSauce) {
+                        console.error("L'utilisateur a déjà liké cette sauce !");
+                    }
+                    else {
+                        sauce.likes += 1;
+                        sauce.usersLiked.push(userId);
+                    }
+                    if (hasTheUserDislikedThisSauce) {
+                        console.error("L'utilisateur doit d'abord annuler son dislike !");
+                    }
+                    break;
+                
+                case 0:
+                    if (hasTheUserLikedThisSauce) {
+                        sauce.likes -= 1;
+                        sauce.usersLiked.filter(id => id !== userId);
+                    }
+                    else {
+                        if (hasTheUserDislikedThisSauce) {
+                            sauce.dislikes -= 1;
+                            sauce.usersDisliked.filter(id => id !== userId)
+                        }
+                    }
+                break;
+                    
+                case -1:
+                    if (hasTheUserDislikedThisSauce) {
+                        console.error("L'utilisateur a déjà disliké la sauce !");
+                    }
+                    else {
+                        sauce.dislikes += 1;
+                        sauce.usersDisliked.push(userId);
+                    }
+                    if (hasTheUserLikedThisSauce) {
+                        console.error("L'utilisateur doit d'abord annuler son like !");
+                    }
+                break;
+
+                default:
+                    console.log('Erreur !');
+            }
+
+            sauce.save()
+                .then(() => res.status(201).json({message: 'Statut like/dislike mis à jour !'}))
+                .catch(error => res.status(400).json({error}));
+        })
+    .catch(error => res.status(500).json({error}));
+
+    // switch (like) {
+    //     case 1:
+    //         Sauce.updateOne({_id: sauceId}, {$push: {usersLiked: userId}, $set: {likes: 1}})
+    //             .then(() => res.status(200).json({message: 'Sauce likée !'}))
+    //             .catch(error => res.status(400).json({error}));
+    //         break;
+        
+    //     case 0:
+    //         Sauce.findOne({_id: sauceId})
+    //             .then((sauce) => {
+    //                 if (sauce.usersLiked.includes(userId)) {
+    //                     Sauce.updateOne({_id: sauceId}, {$pull: {usersLiked: userId}}, {$inc: {likes: -1}})
+    //                         .then(() => res.status(200).json({message: 'Sauce unlikée !'}))
+    //                         .catch(error => res.status(400).json({error}));
+    //                 }
+    //                 if (sauce.usersDisliked.includes(userId)) {
+    //                     Sauce.updateOne({_id: sauceId}, {$pull: {usersDisliked: userId}}, {$inc: {dislikes: -1}})
+    //                         .then(() => res.status(200).json({message: 'Sauce undislikée !'}))
+    //                         .catch(error => res.status(400).json({error}));
+    //                 }
+    //             })
+    //             .catch(error => res.status(404).json({error}));
+    //         break;
+        
+    //     case -1:
+    //         Sauce.updateOne({_id: sauceId}, {$push: {usersDisliked: userId}}, {$set: {dislikes: 1}})
+    //             .then(() => res.status(200).json({message: 'Sauce dislikée !'}))
+    //             .catch(error => res.status(400).json({error}));
+    //         break;
+    
+    //     default:
+    //         console.log('Pas marché');
+    // }
+}
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -9,5 +9,6 @@ router.get('/', auth, sauceCtrl.displaySauces);
 router.get('/:id', auth, sauceCtrl.displaySauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeDislike);
 
 module.exports = router;
